e5de5592e55023395e584bb77635b29a7cb0932b
Luis <luis.eduardo2.developer@gmail.com>
2021-02-19 22:08:04 -0300

    a

--- pages/api/tensei/nezuko.js
+++ pages/api/tensei/nezuko.js
@@ -55,7 +55,7 @@ async function animesEvaluate(page){
         });
         //Retorno da função
         return {
-            "animes":dados,
+            dados,
             "nextpage": nextpage??''
         }
     })
