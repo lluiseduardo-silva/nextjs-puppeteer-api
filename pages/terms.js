3f685de2192f21ee0d8c387d962f31e0164abed1
LeapOfFaith01 <luis.eduardo2.developer@gmail.com>
2023-05-06 19:18:18 -0300

    Terms page update

--- pages/terms.js
+++ pages/terms.js
@@ -1,17 +1,17 @@
 export default function Terms(){
     return(
         <div>
-            <p>By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to To Spen Time.</p>
+            <p>By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to To Spend Time.</p>
             <p>To Spend Time is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.</p>
-            <p>The Yamete Kudasai app no process or store personal data. It’s your responsibility to keep your phone and access to the app secure.</p>
-            <p>We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Yamete Kudasai app won’t work properly or at all.</p>
-            <p>The app does use third party services that declare their own Terms and Conditions.
-Link to Terms and Conditions of third party service providers used by the app</p>
-<a href="https://www.google.com/adsense/new/localized-terms?hl=pt_BR">
+            <p>The app no process or store personal data. It’s your responsibility to keep your phone and access to the app secure.</p>
+            <p>We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the app won’t work properly or at all.</p>
+            {/* <p>The app does use third party services that declare their own Terms and Conditions. */}
+{/* Link to Terms and Conditions of third party service providers used by the app</p> */}
+{/* <a href="https://www.google.com/adsense/new/localized-terms?hl=pt_BR">
     Admob
 </a>
 <br/>
-<a href="https://play.google.com/about/developer-distribution-agreement.html">Google Play Services</a>
+<a href="https://play.google.com/about/developer-distribution-agreement.html">Google Play Services</a> */}
 <p>You should be aware that there are certain things that To Spend Time will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but To Spend Time cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
 If you’re using the app outside of an area with Wi-Fi, you should remember that your terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.</p>
 <p>Along the same lines, To Spend Time cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, To Spend Time cannot accept responsibility.</p>
